5d9afbf4f2838cfcaeaa18d651aff2e993aaa020
gogo1667 <gogo1667@gmail.com>
2026-02-13 14:41:41 -0600

    whale balls

--- game.js
+++ game.js
@@ -12,17 +12,18 @@ canvas.height = window.innerHeight;
 // ----------------------
 
 let hitCount = 0;
+let hitFlashTimer = 0;
 
 // ----------------------
 // NEPHEW
 // ----------------------
 
 let nephew = {
-  x: canvas.width / 2 - 45,
-  y: canvas.height - 90 - 10,
-  width: 90,
-  height: 90,
-  speed: 7
+    x: canvas.width / 2 - 45,
+    y: canvas.height - 90 - 10,
+    width: 90,
+    height: 90,
+    speed: 7
 };
 
 
@@ -30,30 +31,30 @@ let nephew = {
 let keys = {};
 
 document.addEventListener("keydown", e => {
-  keys[e.key] = true;
+    keys[e.key] = true;
 });
 
 document.addEventListener("keyup", e => {
-  keys[e.key] = false;
+    keys[e.key] = false;
 });
 
 function moveNephew() {
-  if (keys["ArrowLeft"] && nephew.x > 0) {
-    nephew.x -= nephew.speed;
-  }
-  if (keys["ArrowRight"] && nephew.x + nephew.width < canvas.width) {
-    nephew.x += nephew.speed;
-  }
+    if (keys["ArrowLeft"] && nephew.x > 0) {
+        nephew.x -= nephew.speed;
+    }
+    if (keys["ArrowRight"] && nephew.x + nephew.width < canvas.width) {
+        nephew.x += nephew.speed;
+    }
 }
 
 function drawNephew() {
-  ctx.drawImage(
-    nephewImg,
-    nephew.x,
-    nephew.y,
-    nephew.width,
-    nephew.height
-  );
+    ctx.drawImage(
+        nephewImg,
+        nephew.x,
+        nephew.y,
+        nephew.width,
+        nephew.height
+    );
 }
 
 
@@ -65,54 +66,54 @@ let whales = [];
 
 function spawnWhale() {
 
-  let whale = {
-    x: Math.random() * (canvas.width - 60),
-    y: 0,
-    width: 60,
-    height: 40,
-    state: "warning",
-    timer: 60
-  };
+    let whale = {
+        x: Math.random() * (canvas.width - 60),
+        y: 0,
+        width: 60,
+        height: 40,
+        state: "warning",
+        timer: 60
+    };
 
-  whales.push(whale);
+    whales.push(whale);
 }
 
 setInterval(spawnWhale, 2000);
 
 function moveWhales() {
 
-  whales.forEach(whale => {
+    whales.forEach(whale => {
 
-    if (whale.state === "warning") {
-      whale.timer--;
+        if (whale.state === "warning") {
+            whale.timer--;
 
-      if (whale.timer <= 0) {
-        whale.state = "falling";
-      }
-    }
+            if (whale.timer <= 0) {
+                whale.state = "falling";
+            }
+        }
 
-    else if (whale.state === "falling") {
-      whale.y += 10;
-    }
+        else if (whale.state === "falling") {
+            whale.y += 10;
+        }
 
-  });
+    });
 
-  whales = whales.filter(whale => whale.y < canvas.height);
+    whales = whales.filter(whale => whale.y < canvas.height);
 }
 
 function drawWhales() {
 
-  whales.forEach(whale => {
+    whales.forEach(whale => {
 
-    if (whale.state === "warning") {
-      ctx.fillStyle = "yellow";
-    } else {
-      ctx.fillStyle = "cyan";
-    }
+        if (whale.state === "warning") {
+            ctx.fillStyle = "yellow";
+        } else {
+            ctx.fillStyle = "cyan";
+        }
 
-    ctx.fillRect(whale.x, whale.y, whale.width, whale.height);
+        ctx.fillRect(whale.x, whale.y, whale.width, whale.height);
 
-  });
+    });
 }
 
 // ----------------------
@@ -121,34 +122,35 @@ function drawWhales() {
 
 function detectHit() {
 
-  for (let i = whales.length - 1; i >= 0; i--) {
+    for (let i = whales.length - 1; i >= 0; i--) {
 
-    let whale = whales[i];
+        let whale = whales[i];
 
-    if (
-      nephew.x < whale.x + whale.width &&
-      nephew.x + nephew.width > whale.x &&
-      nephew.y < whale.y + whale.height &&
-      nephew.y + nephew.height > whale.y
-    ) {
+        if (
+            nephew.x < whale.x + whale.width &&
+            nephew.x + nephew.width > whale.x &&
+            nephew.y < whale.y + whale.height &&
+            nephew.y + nephew.height > whale.y
+        ) {
 
-      // REMOVE WHALE
-      whales.splice(i, 1);
+            whales.splice(i, 1);
+            hitCount++;
 
-      // INCREMENT COUNTER
-      hitCount++;
+            // SHOW "WHALE BALLS!" FOR ~1 SECOND
+            hitFlashTimer = 60;
+        }
     }
-  }
 }
+
 
 // ----------------------
 // DRAW COUNTER
 // ----------------------
 
 function drawCounter() {
-  ctx.fillStyle = "white";
-  ctx.font = "30px Arial";
-  ctx.fillText("Whales Hit: " + hitCount, 20, 40);
+    ctx.fillStyle = "white";
+    ctx.font = "30px Arial";
+    ctx.fillText("Whales Hit: " + hitCount, 20, 40);
 }
 
 // ----------------------
@@ -157,17 +159,61 @@ function drawCounter() {
 
 function gameLoop() {
 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  moveNephew();
-  moveWhales();
-  detectHit();
+    moveNephew();
+    moveWhales();
+    detectHit();
 
-  drawNephew();
-  drawWhales();
-  drawCounter();
+    drawNephew();
+    drawWhales();
+    drawCounter();
+    drawHitFlash();
 
-  requestAnimationFrame(gameLoop);
+
+    requestAnimationFrame(gameLoop);
 }
+
+function drawHitFlash() {
+
+  if (hitFlashTimer > 0) {
+
+    ctx.save();
+
+    let boxWidth = 400;
+    let boxHeight = 150;
+
+    let boxX = canvas.width / 2 - boxWidth / 2;
+    let boxY = canvas.height / 2 - boxHeight / 2;
+
+    // DRAW WHITE BOX
+    ctx.fillStyle = "white";
+    ctx.fillRect(boxX, boxY, boxWidth, boxHeight);
+
+    // DRAW BLACK BORDER
+    ctx.lineWidth = 6;
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(boxX, boxY, boxWidth, boxHeight);
+
+    // DRAW TEXT
+    ctx.fillStyle = "black";
+    ctx.font = "bold 40px Arial";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+
+    ctx.fillText(
+      "WHALE BALLS!",
+      canvas.width / 2,
+      canvas.height / 2
+    );
+
+    ctx.restore();
+
+    hitFlashTimer--;
+  }
+}
+
+
+
 
 gameLoop();
